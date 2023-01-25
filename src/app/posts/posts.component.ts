8fc1a167129b0e9e86602b0ca23c72650732ceca
Ivan Epou <ivanepou92@gmail.com>
2023-01-25 12:20:14 +0300

    Use the ngFor  directive

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { Post } from '../models/Post';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
-  title: string = 'Posts';
+  title: string = 'Post List';
+  posts: Post[] = [];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    //initializing components with some data
+    this.posts = [
+      {
+        id: 1,
+        title: 'First Post',
+        body: 'Test',
+        votes: 1
+      },
+      {
+        id: 2,
+        title: 'Test 2',
+        body: 'Test 2',
+        votes: 10
+      },
+      {
+        id: 3,
+        title: 'Test 3',
+        body: 'Test 3',
+        votes: 10
+      }
+    ]
+  }
 }
